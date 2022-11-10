8a402f1210d12732b015c82d73bc377340e81a6a
ykf158 <1582540778@qq.com>
2022-11-10 17:42:38 +0800

    Update juejin-zlx.js

--- juejin-zlx.js
+++ juejin-zlx.js
@@ -11,7 +11,9 @@ const config = {
         "getTodayStatus": "/growth_api/v1/get_today_status",
         "checkIn": "/growth_api/v1/check_in",
         "getLotteryConfig": "/growth_api/v1/lottery_config/get",
-        "drawLottery": "/growth_api/v1/lottery/draw"
+        "drawLottery": "/growth_api/v1/lottery/draw",
+        "global_big": "/growth_api/v1/lottery_history/global_big?aid=2608&uuid=7108547671534667271&spider=0",
+        "dip_lucky": "/growth_api/v1/lottery_lucky/dip_lucky?aid=2608&uuid=7108547671534667271&spider=0",
     },
     "cookie": '_ga=GA1.2.1735536519.1655087666; _tea_utm_cache_2608=undefined; __tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227108547671534667271%2522%252C%2522user_unique_id%2522%253A%25227108547671534667271%2522%252C%2522timestamp%2522%253A1655087665957%257D; MONITOR_WEB_ID=2283d21c-2147-49b4-acfb-d884e456c7df; _gid=GA1.2.559944146.1667985478; passport_csrf_token=da0334ac4da3de435b65359e2f3a1d30; passport_csrf_token_default=da0334ac4da3de435b65359e2f3a1d30; _tea_utm_cache_2018=undefined; odin_tt=4ad794e9ccfc16f78eaaead5560a25e7c9c2c03eb598e0544178b6d3a761e87ca450e858e58fa195183e9b8aca455d97c45a883a1431b2941f5c5ca950440108; n_mh=-I6iTqUBvSyp2DGZ0x000aa5mbO81oQYteUIUdkIAjw; passport_auth_status=cfff7babaac3324848e120fe3eec2a2c%2C; passport_auth_status_ss=cfff7babaac3324848e120fe3eec2a2c%2C; uid_tt=127e80f8bf5fb9205fac6e7df867e918; uid_tt_ss=127e80f8bf5fb9205fac6e7df867e918; sid_tt=c7fc87f6274787500e8659eb8700573a; sessionid=c7fc87f6274787500e8659eb8700573a; sessionid_ss=c7fc87f6274787500e8659eb8700573a; sid_guard=c7fc87f6274787500e8659eb8700573a%7C1667985591%7C31536000%7CThu%2C+09-Nov-2023+09%3A19%3A51+GMT; sid_ucp_v1=1.0.0-KDVkNDIyNGU0NmE2NDFjMjBhYTZlNGIxMWJlMTM3N2JlZjZjMmQ4YTcKFwidspC464yLBBC34a2bBhiwFDgCQPIHGgJsZiIgYzdmYzg3ZjYyNzQ3ODc1MDBlODY1OWViODcwMDU3M2E; ssid_ucp_v1=1.0.0-KDVkNDIyNGU0NmE2NDFjMjBhYTZlNGIxMWJlMTM3N2JlZjZjMmQ4YTcKFwidspC464yLBBC34a2bBhiwFDgCQPIHGgJsZiIgYzdmYzg3ZjYyNzQ3ODc1MDBlODY1OWViODcwMDU3M2E',
     "email": {
@@ -75,6 +77,31 @@ const getTodayDrawStatus = async () => {
     }
 }
 
+// 查询可以沾喜气的记录
+const getGlobalBig = async () => {
+    const {cookie, baseUrl, apiUrl} = config;
+    let {data} = await axios({url: baseUrl + apiUrl.global_big, method: 'post', data: {page_no: 2, page_size: 5}, headers: {Cookie: cookie}});
+    if (data.err_no) {
+        return {error: true, history_id: null}
+    } else {
+        let lotteries = data.data.lotteries.filter(item => item.dip_lucky_user_count === 0) || data.data.lotteries[0]
+        return {error: false, history_id: lotteries[0].history_id}
+    }
+}
+
+// 沾喜气
+const dipLucky = async () => {
+    let {error, history_id} = await getGlobalBig();
+    if (error) return console.log('查询沾喜气的记录失败');
+    if (!history_id) return console.log('无可沾喜气的记录');
+    const {cookie, baseUrl, apiUrl} = config;
+    let {data} = await axios({url: baseUrl + apiUrl.dip_lucky, method: 'post', data: {lottery_history_id: history_id}, headers: {Cookie: cookie}});
+    if (data.err_no) return console.log('沾喜气失败');
+    console.log(`沾喜气结果has_dip：${data.data.has_dip}`);
+    console.log(`恭喜沾到喜气：${data.data.dip_value}, 目前幸运值${data.data.total_value} / 6000`)
+    await sendEmailFromQQ('沾喜气：成功', JSON.stringify(data));
+}
+
 /*---------------邮件-----------------*/
 
 // 通过qq邮箱发送
@@ -104,6 +131,7 @@ const start = async () => {
     console.log('开始');
     await checkIn();
     await draw();
+    await dipLucky();
     console.log('结束');
 }
 
